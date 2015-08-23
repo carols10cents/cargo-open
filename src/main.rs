f61ae05e9b5e0b2e7f524fedc0f8612119940a04
Carol Nichols <carol.nichols@gmail.com>
2015-08-23 13:44:30 -0400

    Use clap to get the crate name and print the current version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("cargo-open")
+                         .about("A third-party cargo extension to allow you to open a dependent crate in your $EDITOR")
+                         .version(&crate_version!()[..])
+                         .arg(Arg::with_name("CRATE")
+                              .help("The name of the crate you would like to open")
+                              .required(true)
+                              .index(1))
+                         .get_matches();
+    println!("Using crate name: {}", matches.value_of("CRATE").unwrap());
 }
