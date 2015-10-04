602b5f523214f9fdb7cff378fa6ffde0205d176f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2015-10-04 11:55:41 -0400

    Move the tests to be unit tests instead of integration tests
    Since these will just be testing the cargo_editor function.

--- src/main.rs
+++ src/main.rs
@@ -97,3 +97,27 @@ fn absolutize(pb: PathBuf) -> PathBuf {
         std::env::current_dir().unwrap().join(&pb.as_path()).clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn check_env_editor() {
+        let editor: Option<&'static str> = option_env!("EDITOR");
+        assert!(editor != None);
+    }
+
+    #[test]
+    fn check_env_cargo_editor() {
+        let cargo_editor: Option<&'static str> = option_env!("CARGO_EDITOR");
+        assert_eq!(cargo_editor, None);
+    }
+
+    #[test]
+    fn check_env_visual() {
+        let visual: Option<&'static str> = option_env!("VISUAL");
+        assert_eq!(visual, None);
+    }
+}
